a74ef1047f2365a4d33cdc0f8de1b2d2084f56e8
skyprogrammingsolutions <skyprogrammingsolutions@gmail.com>
2021-05-07 22:57:03 +0530

    Initial commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,337 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Buy_shop an E-Commerce online Shopping Category Flat Bootstarp responsive Website Template| Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Buy_shop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="resources/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Custom Theme files -->
+<link href="resources/css/style.css" rel='stylesheet' type='text/css' />
+<link href="resources/css/register.css" rel='stylesheet' type='text/css' />
+
+<script src="resources/js/simpleCart.min.js"> </script>
+<!-- Custom Theme files -->
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="resources/js/jquery-1.11.1.min.js"></script>
+<!-- start menu -->
+<link href="resources/css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+</head>
+<body>
+<div class="header_top">
+	<div class="container">
+		<div class="one-fifth column row_1">
+			<span class="selection-box"><select class="domains valid" name="domains">
+		       <option>English</option>
+		       <option>French</option>
+		       <option>German</option>
+		    </select></span>
+         </div>
+         <div class="cssmenu">
+			<ul>
+			    <li class="active"><a href="login.html">My Account</a></li> 
+			</ul>
+		 </div>
+	</div>
+</div>	
+<div class="wrap-box"></div>
+<div class="header_bottom">
+	    <div class="container">
+			<div class="col-xs-8 header-bottom-left">
+				<div class="col-xs-2 logo">
+					<h1><a href="index.html"><span>Buy</span>shop</a></h1>
+				</div>
+				<div class="col-xs-6 menu">
+		            <ul class="megamenu skyblue">
+				      <li class="active grid"><a class="color1" href="index.html">Men</a><div class="megapanel">
+						<div class="row">
+							<div class="col1">
+								<div class="h_nav">
+									<ul>
+										<li><a href="men.html">Accessories</a></li>
+										<li><a href="men.html">Bags</a></li>
+										<li><a href="men.html">Caps & Hats</a></li>
+										<li><a href="men.html">Hoodies & Sweatshirts</a></li>
+										<li><a href="men.html">Jackets & Coats</a></li>
+										<li><a href="men.html">Jeans</a></li>
+										<li><a href="men.html">Jewellery</a></li>
+										<li><a href="men.html">Jumpers & Cardigans</a></li>
+										<li><a href="men.html">Leather Jackets</a></li>
+										<li><a href="men.html">Long Sleeve T-Shirts</a></li>
+										<li><a href="men.html">Loungewear</a></li>
+									</ul>	
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+									<ul>
+										<li><a href="men.html">Shirts</a></li>
+										<li><a href="men.html">Shoes, Boots & Trainers</a></li>
+										<li><a href="men.html">Shorts</a></li>
+										<li><a href="men.html">Suits & Blazers</a></li>
+										<li><a href="men.html">Sunglasses</a></li>
+										<li><a href="men.html">Sweatpants</a></li>
+										<li><a href="men.html">Swimwear</a></li>
+										<li><a href="men.html">Trousers & Chinos</a></li>
+										<li><a href="men.html">T-Shirts</a></li>
+										<li><a href="men.html">Underwear & Socks</a></li>
+										<li><a href="men.html">Vests</a></li>
+									</ul>	
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+									<h4>Popular Brands</h4>
+									<ul>
+										<li><a href="men.html">Levis</a></li>
+										<li><a href="men.html">Persol</a></li>
+										<li><a href="men.html">Nike</a></li>
+										<li><a href="men.html">Edwin</a></li>
+										<li><a href="men.html">New Balance</a></li>
+										<li><a href="men.html">Jack & Jones</a></li>
+										<li><a href="men.html">Paul Smith</a></li>
+										<li><a href="men.html">Ray-Ban</a></li>
+										<li><a href="men.html">Wood Wood</a></li>
+									</ul>	
+								</div>												
+							</div>
+						  </div>
+						</div>
+					</li>
+				    <li class="grid"><a class="color2" href="#">Women</a>
+					  <div class="megapanel">
+						<div class="row">
+							<div class="col1">
+								<div class="h_nav">
+									<ul>
+										<li><a href="men.html">Accessories</a></li>
+										<li><a href="men.html">Bags</a></li>
+										<li><a href="men.html">Caps & Hats</a></li>
+										<li><a href="men.html">Hoodies & Sweatshirts</a></li>
+										<li><a href="men.html">Jackets & Coats</a></li>
+										<li><a href="men.html">Jeans</a></li>
+										<li><a href="men.html">Jewellery</a></li>
+										<li><a href="men.html">Jumpers & Cardigans</a></li>
+										<li><a href="men.html">Leather Jackets</a></li>
+										<li><a href="men.html">Long Sleeve T-Shirts</a></li>
+										<li><a href="men.html">Loungewear</a></li>
+									</ul>	
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+									<ul>
+										<li><a href="men.html">Shirts</a></li>
+										<li><a href="men.html">Shoes, Boots & Trainers</a></li>
+										<li><a href="men.html">Shorts</a></li>
+										<li><a href="men.html">Suits & Blazers</a></li>
+										<li><a href="men.html">Sunglasses</a></li>
+										<li><a href="men.html">Sweatpants</a></li>
+										<li><a href="men.html">Swimwear</a></li>
+										<li><a href="men.html">Trousers & Chinos</a></li>
+										<li><a href="men.html">T-Shirts</a></li>
+										<li><a href="men.html">Underwear & Socks</a></li>
+										<li><a href="men.html">Vests</a></li>
+									</ul>	
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+									<h4>Popular Brands</h4>
+									<ul>
+										<li><a href="men.html">Levis</a></li>
+										<li><a href="men.html">Persol</a></li>
+										<li><a href="men.html">Nike</a></li>
+										<li><a href="men.html">Edwin</a></li>
+										<li><a href="men.html">New Balance</a></li>
+										<li><a href="men.html">Jack & Jones</a></li>
+										<li><a href="men.html">Paul Smith</a></li>
+										<li><a href="men.html">Ray-Ban</a></li>
+										<li><a href="men.html">Wood Wood</a></li>
+									</ul>	
+								</div>												
+							</div>
+						  </div>
+						</div>
+			    </li>
+				<li><a class="color4" href="about.blade.php">About</a></li>
+				<li><a class="color6" href="register">Register</a></li>
+				<li><a class="color6" href="login">Login</a></li>			
+				<li><a class="color5" href="404.html">Blog</a></li>
+				<li><a class="color6" href="contact.blade">Support</a></li>
+			  </ul> 
+			</div>
+		</div>
+	    <div class="col-xs-4 header-bottom-right">
+	       <div class="box_1-cart">
+		     <div class="box_11"><a href="checkout.html">
+		      <h4><p>Cart: <span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</p><img src="resources/images/bag.png" alt=""/><div class="clearfix"> </div></h4>
+		      </a></div>
+	          <p class="empty"><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+	          <div class="clearfix"> </div>
+	        </div>
+	        <div class="search">	  
+				<input type="text" name="s" class="textbox" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+				<input type="submit" value="Subscribe" id="submit" name="submit">
+				<div id="response"> </div>
+		     </div>
+	         <div class="clearfix"></div>
+       </div>
+        <div class="clearfix"></div>
+	 </div>
+</div>
+<div class="banner">
+	<div class="container">
+		<div class="banner_desc">
+			<h1>New Season Arrivals.</h1>
+			<h2>Check out all the new trends</h2>
+			<div class="button">
+			      <a href="#" class="hvr-shutter-out-horizontal">Shop Now</a>
+			    </div>
+		</div>
+	</div>
+</div>	
+<!--Registration form -->
+<div class="form-content">
+	<div class="container">
+		<div class="heading">
+			<h1>Register yourself</h1>
+		</div>
+		<form action="/register_code" method="post" enctype="multipart/form-data">
+			<div class="form-group">
+				<label for="name">Name</label>
+				<input type="text" class="form-control" id="name" name="name">
+			</div>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="text" class="form-control" id="email" name="email">
+			</div>
+			<div class="form-group">
+				<label for="mobile">Mobile No</label>
+				<input type="number" class="form-control" id="number" name="number">
+			</div>
+			<div class="form-group">
+				<label for="pwd">Password:</label>
+				<input type="password" class="form-control" id="pwd" name="pwd">
+			</div>
+			<div class="form-group">
+				<label for="cpwd">Confirm Password:</label>
+				<input type="password" class="form-control" id="cpwd" name="cpwd">
+			</div>
+			<div class="form-group">
+				<label for="gender">Gender</label>
+				<input type="radio"  id="gender" name="gender" value="male">Male
+				<input type="radio"  id="gender" name="gender" value="female">Female
+			</div>
+			<div class="form-group">
+				<label for="Hobbies">Hobbies:</label>
+				<input type="checkbox" class="hoby"  id="hoby1" name="hobbie[]">Dancing
+				<input type="checkbox" class="hoby"  id="hoby2" name="hobbie[]">Singing
+				<input type="checkbox" class="hoby" id="hoby3" name="hobbie[]">Reading
+				<input type="checkbox" class="hoby" id="hoby4" name="hobbie[]">Playing
+			</div>
+			<div class="form-group">
+				<label for="file">Select Picture:</label>
+				<input type="file" class="form-control" id="file" name="file">
+			</div>
+			<div class="form-group">
+				<label for="state">State</label>
+				<select name="state" id="state">
+					<option>Select state</option>
+					<option>Haryana</option>
+					<option>UP</option>
+				</select>
+			</div>
+			
+			<div class="form-group">
+				<label for="city">City</label>
+				<select name="city" id="city">
+					<option>select city</option>
+					<option>Gonda</option>
+					<option>Kanpur</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="address">Address</label>
+				<textarea name="address" id="address"></textarea>
+			</div>
+			<div class="checkbox">
+				<label><input type="checkbox" id="remember" name="remember"> Remember me</label>
+			</div>
+  				<button type="submit" class="btn btn-default" name="submit">Register Now</button>
+		</form>
+	</div>
+</div>
+<div class="footer">
+	<div class="container">
+	   <div class="footer_top">
+		<div class="col-md-4 box_3">
+			<h3>Our Stores</h3>
+			<address class="address">
+              <p>9870 St Vincent Place, <br>Glasgow, DC 45 Fr 45.</p>
+              <dl>
+                 <dt></dt>
+                 <dd>Freephone:<span> +1 800 254 2478</span></dd>
+                 <dd>Telephone:<span> +1 800 547 5478</span></dd>
+                 <dd>FAX: <span>+1 800 658 5784</span></dd>
+                 <dd>E-mail:&nbsp; <a href="mailto@example.com">info(at)buyshop.com</a></dd>
+              </dl>
+           </address>
+           <ul class="footer_social">
+			  <li><a href=""> <i class="fb"> </i> </a></li>
+			  <li><a href=""><i class="tw"> </i> </a></li>
+			  <li><a href=""><i class="google"> </i> </a></li>
+			  <li><a href=""><i class="instagram"> </i> </a></li>
+		   </ul>
+		</div>
+		<div class="col-md-4 box_3">
+			<h3>Blog Posts</h3>
+			<h4><a href="#">Sed ut perspiciatis unde omnis</a></h4>
+			<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced</p>
+			<h4><a href="#">Sed ut perspiciatis unde omnis</a></h4>
+			<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced</p>
+			<h4><a href="#">Sed ut perspiciatis unde omnis</a></h4>
+			<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced</p>
+		</div>
+		<div class="col-md-4 box_3">
+			<h3>Support</h3>
+			<ul class="list_1">
+				<li><a href="#">Terms & Conditions</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Payment</a></li>
+				<li><a href="#">Refunds</a></li>
+				<li><a href="#">Track Order</a></li>
+				<li><a href="#">Services</a></li>
+			</ul>
+			<ul class="list_1">
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Press</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="clearfix"> </div>
+		</div>
+		<div class="footer_bottom">
+			<div class="copy">
+                <p>Copyright © 2021 easyday. All Rights Reserved.<a href="#" target="_blank">easyday.com</a> </p>
+	        </div>
+	    </div>
+	</div>
+</div>
+</body>
+</html>		
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\user_controller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/profile',[user_controller::class, 'index']);
+Route::get('/register',[user_controller::class, 'register']);
+Route::get('/login',[user_controller::class, 'login']);
